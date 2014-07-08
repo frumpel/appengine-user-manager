790a644264f9ab3db4e8098cbb3795b4db8f9365
Rudolf Potucek <potucek-github@artiverse.net>
2014-07-08 10:19:45 -0600

    first working sample: information is aggregated in the background page from all apps you view and displayed in the popup window on demand

--- message_read.js
+++ message_read.js
@@ -2,7 +2,14 @@ function get_info() {
 	chrome.runtime.sendMessage(
 		{method:'getUserList'}, 
 		function(response){
-			$('.output').innerText=response;
+			// $('.output').innerText=response;
+			var rs = "Response:<P>" ;
+			var rskeys = Object.keys(response).sort(
+				function (a, b) { return a.toLowerCase().localeCompare(b.toLowerCase())});
+			rskeys.forEach(function(entry){
+				rs += entry.toString() + "<BR>";
+			})
+			document.getElementsByClassName("output")[0].innerHTML=rs;
 		});
 };
 
